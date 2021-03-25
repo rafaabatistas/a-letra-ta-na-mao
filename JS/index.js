c1bd9afa3ada33c07f1ff18d22df2df40a932c86
Rafael <rafaabatistas@gmail.com>
2021-03-25 17:33:29 -0300

    Creating the first version of the script in JS

--- JS/index.js
+++ JS/index.js
@@ -13,31 +13,36 @@ form.addEventListener('submit', el => {
     doSubmit();
 })
 
-async function doSubmit() {
-    const boxLetra = document.querySelector('#box-lyrics') 
+ function doSubmit() {
+    const caixa = document.querySelector('.caixa');
+    const boxLetra = document.querySelector('#box-lyrics');
+    const title = document.querySelector('.title'); 
     const letra = document.querySelector('.lyrics');
     const artista = document.querySelector('#artista');
     const music = document.querySelector('#music');
 
     boxLetra.innerHTML = '<img src="SVG/loader.svg" alt="loading...">';
-    // then
-    // procurarLetra(artista.value, music.value)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         if(data.lyrics) {
-    //             // boxLetra.classList('.box-lyrics');
-    //             letra.innerHTML = data.lyrics;
-    //         } else {
-    //             letra.innerHTML = data.error;
-    //         }
-    //     })
-    //     .catch(err => {
-    //         letra.innerHTML = `Opss ${err}`;
-    //     })
+    
+    procurarLetra(artista.value, music.value)
+        .then(response => response.json())
+        .then(data => {
+            if(data.lyrics) {
+                var nameMusic = music.value; 
+                caixa.classList.add('box-lyrics');
+                boxLetra.innerHTML = '';
+                title.innerText = nameMusic.toUpperCase();
+                letra.innerHTML = data.lyrics;
+            } else {
+                letra.innerText = data.error;
+            }
+        })
+        .catch(err => {
+            console.log(`Opss ${err}`);
+        })
 
     // async await
 
-    const lyricResponse = await procurarLetra(artista.value, music.value );
-    const data = await lyricResponse.json();
-    letra.innerHTML = data.lyrics;
+    // const lyricResponse = await procurarLetra(artista.value, music.value );
+    // const data = await lyricResponse.json();
+    // letra.innerText = data.lyrics;
 }

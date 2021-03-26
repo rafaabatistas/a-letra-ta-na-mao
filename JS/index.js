95a6c8ccdb5cbc5c5f697d8db9d0b1a4ff01e036
Rafael <rafaabatistas@gmail.com>
2021-03-26 15:47:28 -0300

    Update script for JS

--- JS/index.js
+++ JS/index.js
@@ -1,48 +1,54 @@
+// Consts
+const caixa = document.querySelector('.caixa');
+const boxLoading = document.querySelector('#box-loading');
+const title = document.querySelector('.title'); 
+const letra = document.querySelector('.lyrics');
+const artista = document.querySelector('#artista');
+const music = document.querySelector('#music');
+    
 // URL da API
-
 function procurarLetra(artista, musica) {
-    return fetch(`https://api.lyrics.ovh/v1/${artista}/${musica}`);
-    // https://api.lyrics.ovh/v1/xuxa/lua_de_cristal
+       return fetch(`https://api.lyrics.ovh/v1/${artista}/${musica}`);
 }
 
 // Cancelando a função de enviar padrão do button
-
 const form = document.querySelector('.text-area');
 form.addEventListener('submit', el => {
     el.preventDefault();
     doSubmit();
 })
 
- function doSubmit() {
-    const caixa = document.querySelector('.caixa');
-    const boxLetra = document.querySelector('#box-lyrics');
-    const title = document.querySelector('.title'); 
-    const letra = document.querySelector('.lyrics');
-    const artista = document.querySelector('#artista');
-    const music = document.querySelector('#music');
 
-    boxLetra.innerHTML = '<img src="SVG/loader.svg" alt="loading...">';
+ function doSubmit() {
+    boxLoading.innerHTML = '';
+    title.innerText = '';
+    caixa.classList.remove('box-lyrics');
+    title.style.display = "none";
+    letra.style.display = "none";
+    boxLoading.innerHTML = '<img src="SVG/loader.svg" alt="loading...">';
     
     procurarLetra(artista.value, music.value)
         .then(response => response.json())
         .then(data => {
             if(data.lyrics) {
+                title.style.display = "block";
+                letra.style.display = "block";
                 var nameMusic = music.value; 
                 caixa.classList.add('box-lyrics');
-                boxLetra.innerHTML = '';
+                boxLoading.innerHTML = '';
                 title.innerText = nameMusic.toUpperCase();
                 letra.innerHTML = data.lyrics;
             } else {
-                letra.innerText = data.error;
+                boxLoading.innerHTML = '<img src="IMG/animatio-not-found.gif" alt="not found">';
+                title.style.display = "block";
+                letra.style.display = "block";
+                title.innerText = 'Desculpe, problema inesperado:(';
             }
         })
         .catch(err => {
-            console.log(`Opss ${err}`);
+            boxLoading.innerHTML = '<img src="IMG/animatio-not-found.gif" alt="not found">';
+            title.style.display = "block";
+            letra.style.display = "block";
+            title.innerText = 'Desculpe, problema inesperado :(';
         })
-
-    // async await
-
-    // const lyricResponse = await procurarLetra(artista.value, music.value );
-    // const data = await lyricResponse.json();
-    // letra.innerText = data.lyrics;
 }

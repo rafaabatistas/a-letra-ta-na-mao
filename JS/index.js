9574cb4dc961ece5e3e04d25321411b0d339593a
Rafael <rafaabatistas@gmail.com>
2021-09-11 20:46:01 -0300

    Making use of a new API

--- JS/index.js
+++ JS/index.js
@@ -8,7 +8,7 @@ const music = document.querySelector('#music');
     
 // URL da API
 function procurarLetra(artista, musica) {
-       return fetch(`https://api.lyrics.ovh/v1/${artista}/${musica}`);
+    return fetch(`https://api.vagalume.com.br/search.php?art=${artista}&mus=${musica}&apikey=7a44796f0d4bba952b8d3936ffe32dd9`);
 }
 
 // Cancelando a função de enviar padrão do button
@@ -18,8 +18,7 @@ form.addEventListener('submit', el => {
     doSubmit();
 })
 
-
- function doSubmit() {
+const doSubmit = () => {
     boxLoading.innerHTML = '';
     title.innerText = '';
     letra.innerHTML = '';
@@ -27,18 +26,19 @@ form.addEventListener('submit', el => {
     title.style.display = "none";
     letra.style.display = "none";
     boxLoading.innerHTML = '<img src="SVG/loader.svg" alt="loading...">';
-    
+
     procurarLetra(artista.value, music.value)
         .then(response => response.json())
         .then(data => {
-            if(data.lyrics) {
+            console.log(data.mus[0])
+            if(data.mus[0]) {
                 title.style.display = "block";
                 letra.style.display = "block";
-                var nameMusic = music.value; 
+                var nameMusic = data.mus[0].name; 
                 caixa.classList.add('box-lyrics');
                 boxLoading.innerHTML = '';
                 title.innerText = nameMusic.toUpperCase();
-                letra.innerHTML = data.lyrics;
+                letra.innerHTML = data.mus[0].text;
             } else {
                 boxLoading.innerHTML = '<img src="IMG/animatio-not-found.gif" alt="not found">';
                 title.style.display = "block";
